308971d43ca8bda3ccb359d5cc6dca89220f7dbb
Tcheny <eugenie.be@hotmail.fr>
2017-08-25 19:20:52 +0200

    update comment

--- routes.js
+++ routes.js
@@ -6,7 +6,7 @@ const router   = express.Router(); // gestionnaire de route
 
 // Afficher la liste de notre DataBase mongodb
 // ======================
-router.get('/', (req, res) => { // get prendre
+router.get('/', (req, res) => { // 'get' prendre
   Task.find((error, tasks) => { // Task.find trouve tous les éléments de la DB
     res.render('homepage', { tasks }); // "render" fabrique une vue a partir de pug
     // console.log(tasks);
@@ -27,21 +27,21 @@ router.post('/create', (req, res) => { // 'post' le client envoi au server
 // 1- recuperer les infos
 router.get("/:id/edit", (req, res) => { // prend les infos du chemin
   Task.findById(req.params.id, (error, task) => { // trouver par ID l'objet en entier
-    res.render('edit', { task }); // puis l'envoyer sur la page edit et affiche la tache selectioné
+    res.render('edit', { task }); // puis l'envoyer sur la page edit et afficher la tache selectioné
   });
 });
 // 2- faire l'update
-router.post("/:id/edit", (req, res) => { // action de soumettre la modif
-  Task.findByIdAndUpdate(req.params.id, req.body, (error) => { // trouve objet id, update le corps de l'objet
-  res.redirect('/'); // refaire un get vers la racine
+router.post("/:id/edit", (req, res) => { // le client de soumettre la modification
+  Task.findByIdAndUpdate(req.params.id, req.body, (error) => { // trouver par ID l'objet en entier + update le contenu de l'objet
+  res.redirect('/'); // refait un get vers la racine
   })
 });
 
 // Supprimer une tache
 // ======================
 router.get("/:id/delete", (req, res) => {
-  Task.findByIdAndRemove(req.params.id, (error) => { // trouver objet id, et le supprime directement
-    res.redirect('/'); // refaire un get vers la racine
+  Task.findByIdAndRemove(req.params.id, (error) => { // trouver par ID l'objet en entier et le supprime directement
+    res.redirect('/'); // refait un get vers la racine
   })
 });
 
